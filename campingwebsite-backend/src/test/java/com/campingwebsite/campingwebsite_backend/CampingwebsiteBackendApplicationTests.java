4e853483cf6248809faaf3e9a9967e718b479cb7
ANASTAD2000 <anasbaclibr@gmai.com>
2025-06-17 11:47:53 +0100

    first commit

---
+++ campingwebsite-backend/src/test/java/com/campingwebsite/campingwebsite_backend/CampingwebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campingwebsite.campingwebsite_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingwebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
