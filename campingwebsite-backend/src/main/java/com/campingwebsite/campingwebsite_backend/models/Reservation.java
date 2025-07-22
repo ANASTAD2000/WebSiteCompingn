94649738c731be8852ba72550a78c04ed4eeddfa
ANASTAD2000 <anasbaclibr@gmai.com>
2025-07-22 11:02:58 +0100

    Base du projet

---
+++ campingwebsite-backend/src/main/java/com/campingwebsite/campingwebsite_backend/models/Reservation.java
@@ -0,0 +1,34 @@
+package com.campingwebsite.campingwebsite_backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private double prix;
+
+    private String lieu;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "guide_id")
+//    private Guide guide;
+
+}
